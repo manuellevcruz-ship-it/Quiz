96d7973ff1e596bd9737a2e014cde950f9be095d
manuellevcruz-ship-it <manuellevcruz@gmail.com>
2026-01-16 00:38:10 +0800

    Add files via upload

---
+++ Javascript/questions.js
@@ -0,0 +1,32 @@
+const questions = [
+    {
+        category: "Physic",
+        questions: [
+            {
+                questions: "What is the capital of the Philippines?",
+                options: ["Manila", "Cebu", "Davao", "Baguio"],
+                correctAnswer: 0,
+            },
+            {
+                questions: "Who are you?",
+                options: ["You", "Me", "They", "Them"],
+                correctAnswer: 1,
+            },  
+            {
+                questions: "A student was running late to class and he took a shortcut to reach the school swiftly. The path is 550 meters away from school, and since the student is running late, he runs at a speed of 5m/s. How long will it take for the student to reach the school? Express the time in minutes.?",
+                options: ["1 minute and 30 seconds", "1 minute and 50 seconds", "2 minutes", "2 minutes and 30 seconds"],
+                correctAnswer: 0,
+            }, 
+            {
+                questions: "A marathon runner is running at 2mph and reaches the finish line in 45 minutes. What is the distance between the starting line and the finish line?",
+                options: ["1.1 miles", "1.3 miles", "1.4 miles", "1.5 miles"],
+                correctAnswer: 3,
+            },
+            {
+                questions: "A car travels at 120km in 5 hours. How fast was the car moving? Express the speed in km/h.",
+                options: ["15 km/h", "18 km/h", "21 km/h", "24 km/h"],
+                correctAnswer: 3,
+            },                        
+        ]
+    }
+];
---
+++ Javascript/script.js
@@ -0,0 +1,32 @@
+const answerOptions = document.querySelector(".choices");
+let quizCategory = "Physic";
+
+// Random Questions function
+const getRandomQuestion = () => {
+    const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions || [];
+
+    const randomQuestion = categoryQuestions[Math.floor(Math.random() * categoryQuestions.length)];
+    return randomQuestion;
+}
+
+// Render the current question and its options in the quiz
+const renderQuestion = () => {
+    const currentQuestion = getRandomQuestion();
+    if(!currentQuestion) return;
+
+    // Update the Ui
+    answerOptions.innerHTML = "";
+    document.querySelector(".question-box").textContent = currentQuestion.questions;
+
+    //Create options <li> elements and append them
+    currentQuestion.options.forEach(option => 
+    {
+        const li = document.createElement("li");
+        li.classList.add("choices");
+        li.textContent = option;
+        answerOptions.appendChild(li);
+    });
+
+}
+
+renderQuestion();
